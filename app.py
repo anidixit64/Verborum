36af8b936c859fbef5f04bdc37d6b8320b47765e
Aniket Dixit <anidixit@umich.edu>
2024-08-19 23:45:50 -0500

    Added CRUD routes for making and deleting flashcards

--- app.py
+++ app.py
@@ -1,24 +1,47 @@
-from flask import Flask
-from config import config
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object(config['development'])
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///verborum.db'
 db = SQLAlchemy(app)
 
 class Flashcard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(100), nullable=False)
-    definition = db.Column(db.String(200), nullable=False)
+    front = db.Column(db.String(200), nullable=False)
+    back = db.Column(db.String(200), nullable=False)
 
-    def __repr__(self):
-        return f'<Flashcard {self.word}>'
+# Create a new flashcard
+@app.route('/flashcards', methods=['POST'])
+def create_flashcard():
+    data = request.get_json()
+    new_flashcard = Flashcard(front=data['front'], back=data['back'])
+    db.session.add(new_flashcard)
+    db.session.commit()
+    return jsonify({"message": "Flashcard created!"}), 201
 
-@app.route('/')
-def home():
-    return "Welcome to Verborum - Your SRS Language Learning App!"
+# Get all flashcards
+@app.route('/flashcards', methods=['GET'])
+def get_flashcards():
+    flashcards = Flashcard.query.all()
+    return jsonify([{'id': f.id, 'front': f.front, 'back': f.back} for f in flashcards])
 
-if __name__ == "__main__":
-    app.run()
+# Update a flashcard
+@app.route('/flashcards/<int:id>', methods=['PUT'])
+def update_flashcard(id):
+    flashcard = Flashcard.query.get_or_404(id)
+    data = request.get_json()
+    flashcard.front = data['front']
+    flashcard.back = data['back']
+    db.session.commit()
+    return jsonify({"message": "Flashcard updated!"})
 
+# Delete a flashcard
+@app.route('/flashcards/<int:id>', methods=['DELETE'])
+def delete_flashcard(id):
+    flashcard = Flashcard.query.get_or_404(id)
+    db.session.delete(flashcard)
+    db.session.commit()
+    return jsonify({"message": "Flashcard deleted!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- config.py
+++ config.py
@@ -1,24 +1,7 @@
+# config.py
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///verborum.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///verborum.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = False
-    TESTING = False
 
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-class TestingConfig(Config):
-    TESTING = True
-
-class ProductionConfig(Config):
-    pass
-
-config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig
-}
